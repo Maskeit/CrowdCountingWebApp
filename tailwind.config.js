2fc709fc0b7ac135c5a2794d90d940601610df83
Miguel <miguelagustin182@gmail.com>
2024-12-10 02:24:04 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'], // Incluye todos los archivos relevantes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
